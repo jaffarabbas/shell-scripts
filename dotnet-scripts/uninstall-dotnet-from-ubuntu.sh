2dbe59f1ee2dcf4624d24841162ddc4b8943a5b2
jaffarabbas <gamaportal8@gmail.com>
2022-11-16 14:45:21 +0500

    Update uninstall-dotnet-from-ubuntu.sh

--- dotnet-scripts/uninstall-dotnet-from-ubuntu.sh
+++ dotnet-scripts/uninstall-dotnet-from-ubuntu.sh
@@ -16,3 +16,10 @@ sudo apt-get remove - purge -y dotnet-targeting-pack-6.0
 sudo apt-get remove --purge -y dotnet-apphost-pack-6.0
 sudo apt-get remove --purge -y dotnet-host/now 6.0.0
 sudo apt-get remove - purge -y dotnet-apphost-pack-6.0
+sudo apt-get remove - purge -y dotnet-runtime-7.0 
+sudo apt-get remove - purge -y dotnet-hostfxr-7.0
+sudo apt-get remove - purge -y dotnet-runtime-deps-7.0 
+sudo apt-get remove - purge -y dotnet-targeting-pack-7.0
+sudo apt-get remove --purge -y dotnet-apphost-pack-7.0
+sudo apt-get remove --purge -y dotnet-host/now 7.0.0
+sudo apt-get remove - purge -y dotnet-apphost-pack-7.0
