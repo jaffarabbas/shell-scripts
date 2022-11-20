86a838cbfc980fe1eea780248d38c5ccfe55e3d8
jaffarabbas <gamaportal8@gmail.com>
2022-11-20 20:31:35 +0500

    Create install-jenkins-in-ubuntu.sh

---
+++ jenkins-scripts/install-jenkins-in-ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+sudo apt install figlet
+figlet "INSTALLING JENKINS IN UBUNTU"
+
+
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get install jenkins
